01a03e1e7ff0bfde3b5163ccb9d91dd3fc11af70
Nandita <Nandita@wirelessprvnat-172-16-214-211.near.illinois.edu>
2017-09-26 12:50:22 -0500

    This is Nandita, I finished Lab4!

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,36 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+ * This class is a reclamation project.
+ */
+public class ReclamationProject {
+    /**
+     * This variable has the name doit.
+     * @param a The first word.
+     * @param b The second word.
+     * @return This returns the variable r.
+     */
+    static String doit(final String a, final String b) {
+        if (a.length() > b.length()) {
+            String tempA = a;
+            String tempB = b;
+            tempA = b;
+            tempB = a;
+        }
+            String r = "";
+            /*
+             * For loop with i
+             */
+            for (int i = 0; i < a.length(); i++) {
+                for (int j = a.length() - i; j > 0; j--) {
+                    for (int k = 0; k < b.length() - j; k++) {
+                        if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                            a.substring(i, i + j);
+                        }
+                    }
+               } // Ah yeah
+            }
+            return r;
+    }
 }
